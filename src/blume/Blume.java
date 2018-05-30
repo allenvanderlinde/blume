1a9ef8c02953c99fecde11aa96a3ee75079edc0f
allenvanderlinde <allen.vanderlinde@gmail.com>
2018-05-30 10:35:02 -0400

    Project upload

---
+++ src/blume/Blume.java
@@ -0,0 +1,22 @@
+/**
+ * Project: Blume
+ * 
+ * @brief	Blume is a simple interface for producing ANSI 8- and 24-bit colored text with
+ * foreground and background color options.
+ * 
+ * @file	Blume.java
+ * 
+ * @author	Allen Vanderlinde
+ * @date	5/29/2018
+ * 
+ * @copyright (CC) 2018, Allen Vanderlinde. Attribution 4.0 International (CC BY 4.0). https://creativecommons.org/licenses/by/4.0/
+ */
+
+
+package blume;
+
+public class Blume implements IBlume {
+	public void print( String text ) {
+		
+	}
+}
